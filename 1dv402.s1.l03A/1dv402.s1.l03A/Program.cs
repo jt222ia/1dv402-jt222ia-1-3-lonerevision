3079d834a462d3fad8baee401b1f6ac83ee19d98
jt222ia <jt222ia@student.lnu.se>
2014-09-22 20:16:12 +0200

    Fortsätter att bygga. lagt in arrayen som ska hålla koll på mina inkommande löner

--- 1dv402.s1.l03A/1dv402.s1.l03A/Program.cs
+++ 1dv402.s1.l03A/1dv402.s1.l03A/Program.cs
@@ -11,20 +11,32 @@ namespace _1dv402.s1.l03A
         static void Main(string[] args)
         {
 
+            Salaries = ReadInt("Ange hur många löner som ska beräknas: ");
+            ProcessSalaries(); 
 
-        }
+
+       }
 
         static void ProcessSalaries(int count)
         {
+            int[] salaries = new int[count];
 
-
+            for (int i = 0; i < salaries.Length; i++)
+            {
+                salaries[i] = ReadInt(String.Format("Ange lön nr {0}: ", i + 1));
+            }
+            
         }
 
-        static string ReadInt(string prompt) 
+        static int ReadInt(string prompt) 
         {
-        
+            int input;           
+            Console.Write(prompt);
+            input = int.Parse(Console.ReadLine());
+            return input;
+
         }
-        
+       
       
     }
 }
