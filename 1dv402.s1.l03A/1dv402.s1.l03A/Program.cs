5ffa862c9c87cf2c77baef796518cdad36770948
jt222ia <jt222ia@student.lnu.se>
2014-09-24 12:30:34 +0200

    Nu fungerar programmet som jag vill o Mats vill.....GLAD..svårt med Escape knappen

--- 1dv402.s1.l03A/1dv402.s1.l03A/Program.cs
+++ 1dv402.s1.l03A/1dv402.s1.l03A/Program.cs
@@ -10,24 +10,35 @@ namespace _1dv402.s1.l03A
     {
         static void Main(string[] args)
         {
-
-            int salariesInput = 0;
-            salariesInput = ReadInt("Ange hur många löner som ska beräknas: ");
-            Console.WriteLine();
-            if (salariesInput >= 2)
+            
+            do
             {
-                ProcessSalaries(salariesInput);
-            }
 
-            else
-            {
-                Console.BackgroundColor = ConsoleColor.Red;
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("FEL! Du måste minst ange 2 löner för att beräkna");
-                Console.ResetColor();
-            }
+                int salariesInput = 0;
+                salariesInput = ReadInt("Ange hur många löner som ska beräknas: ");
+                Console.WriteLine();
 
+                if (salariesInput >= 2)
+                {
+                    ProcessSalaries(salariesInput);
+                }
 
+                else
+                {
+                    
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("FEL! Du måste minst ange 2 löner för att beräkna");
+                    Console.ResetColor();
+                }
+                    Console.WriteLine();
+                    Console.BackgroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("Tryck på valfri tangent för att göra en ny beräkning - Escape avslutar");
+                    Console.ResetColor();
+                    Console.WriteLine();
+            } 
+            while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+           
             Console.ReadKey();
 
        }
@@ -39,35 +50,33 @@ namespace _1dv402.s1.l03A
             for (int i = 0; i < totalSalaries.Length; i++)
             {
                 totalSalaries[i] = ReadInt(String.Format("Ange lön nr {0}: ", i + 1));
+               
             }
 
-            Console.WriteLine();
-            
-            
-            //Sorterar Arrayen
-            Array.Sort(totalSalaries);
-
-            //Klonar min array
             //klonar min array
             int[] copy = (int[])totalSalaries.Clone();
 
+            //Sorterar Arrayen
+            Array.Sort(totalSalaries);
+                                
+            //Deklarerar de variablar jag behöver till uträkningen
             int numberOfSalaries = 0;
             int median = 0;
             int avergageSalary = 0;
             int salarySpread = 0;
 
-            //antal löner i arrayen räknas in i antalLoner
+            //Antal löner i arrayen räknas in i antalLoner
             numberOfSalaries = copy.Count();
 
-            //kolla med hjälp av modulus om antal löner e ett jämnt eller ojämnt tal
-            if (numberOfSalaries % 2 == 0)
+            //Kolla med hjälp av modulus om antal löner e ett jämnt eller ojämnt tal
+            if (numberOfSalaries % 3 == 0)
             {
-                // jämnt antal inmatade löner
+                //Jämnt antal inmatade löner
                 median = (copy[(numberOfSalaries / 2) - 1] + copy[(numberOfSalaries / 2)]) / 2;
                             }
             else
             {
-                // udda antal inmatade löner
+                //Udda antal inmatade löner
                 median = copy[((numberOfSalaries - 1) / 2)];
                 
             }
@@ -84,12 +93,11 @@ namespace _1dv402.s1.l03A
             Console.WriteLine("{0, -15} {1, 10:c0}", "Medellön:", avergageSalary);
             Console.WriteLine("{0, -15} {1, 10:c0}", "Lönespridning:", salarySpread);
             Console.WriteLine("---------------------------------");
+            Console.WriteLine();
 
-                
-            
-
+             
             //Skriva ut de heltal som arrayen håller och presenterar 
-            for (int i = 0; i < totalSalaries.Length; ++i)
+            for (int i = 0; i < copy.Length; ++i)
             {
                 
                 if(i % 3 == 0)
@@ -97,20 +105,41 @@ namespace _1dv402.s1.l03A
                     Console.WriteLine();
                     
                 }
-                Console.Write("{0, 8}", totalSalaries[i]);
-            }                        
-            //Escape historien som jag inte fått til riktigt än
-
-            
+                Console.Write("{0, 8}", copy[i]);
+            }                                           
         }
 
         static int ReadInt(string prompt) 
         {
-            int input;           
-            Console.Write(prompt);
-            input = int.Parse(Console.ReadLine());
-            return input;
+           int input;
+           
+           while(true)
+           {
+               try
+               {
 
+                   Console.Write(prompt);
+                   input = int.Parse(Console.ReadLine());
+                   break;
+               }
+               catch (FormatException)
+               {
+                   Console.WriteLine();
+                   Console.BackgroundColor = ConsoleColor.Red;
+                   Console.ForegroundColor = ConsoleColor.White;
+                   Console.WriteLine("FEL! Felaktigt format på inmatningen. Ange ett heltal( 1, 2, 5, 7 tex!");
+                   Console.ResetColor();
+               }
+               catch 
+               {
+                   Console.BackgroundColor = ConsoleColor.Red;
+                   Console.WriteLine("Okänt fel. Försökt igen.");
+                   Console.ResetColor();
+               }
+               Console.WriteLine();
+           }
+           
+           return input;
         }
        
       
